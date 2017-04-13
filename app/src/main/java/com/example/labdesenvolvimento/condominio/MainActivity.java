b0d06db6d2e759084b773dd1187c37025a69a8b5
ChamadoCondominio <barteczko26@gmail.com>
2017-04-13 19:47:12 -0300

    13/04/2017

--- app/src/main/java/com/example/labdesenvolvimento/condominio/MainActivity.java
+++ app/src/main/java/com/example/labdesenvolvimento/condominio/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.labdesenvolvimento.condominio;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+public class  MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectMainOpetion(View view){
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btnInserir:
+                intent = new Intent(this,InsertCondActivity.class);
+                break;
+            case R.id.btnListar:
+                intent = new Intent(this,ListCondActivity.class);
+                break;
+        }
+        if(intent != null){
+            startActivity(intent);
+        }
     }
 }
